65d22a947c5ba891ee7357a2700fc0226a69be81
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-14 07:44:34 +0000

    Placeholder text replacement

--- components/simple_footer_component_1723621464.js
+++ components/simple_footer_component_1723621464.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1723621464", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            CodeAcademy: Learn to Code Interactively
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Master programming skills through hands-on coding challenges and tutorials. Join our community of learners today!
                     </div>
                 </div>
             </div>
